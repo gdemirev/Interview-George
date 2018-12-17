fdd207f600664db8645f08d3f5887f4e1fe51091
gdemirev <gdemirev@usa.net>
2018-12-17 18:12:57 -0500

    Add files via upload

---
+++ Haas-Term.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+
+namespace MM_Telnet
+{
+    class HaasConnection
+    {
+        TcpClient TermSock;
+        public bool IsConnected
+        {
+            get { return TermSock.Connected; }
+        }
+        public HaasConnection(string Hostname, int Port)
+        {
+            TermSock = new TcpClient(Hostname, Port);
+        }
+        public void TermWrite(string cmd)
+        {
+            if (!TermSock.Connected) return;
+            byte[] buf = System.Text.ASCIIEncoding.ASCII.GetBytes(cmd);
+            TermSock.GetStream().Write(buf, 0, buf.Length);
+        }
+        public string TermRead()
+        {
+            int TimeOut = 100;
+            if (!TermSock.Connected) return string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                while (TermSock.Available > 0)
+                {
+                    char input = (char)TermSock.GetStream().ReadByte();
+                    if ((input == '\r') || (input == '\n') || (input >= ' ' && input <= '~'))
+                        // only append printable ASCII
+                        sb.Append((char)input);
+                }
+                System.Threading.Thread.Sleep(TimeOut);
+            }
+            while (TermSock.Available > 0);
+            return sb.ToString();
+        }
+}
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Net.Sockets;
+
+namespace MM_Telnet
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string IPAddr = "127.0.0.1";
+            int Port = 4000;
+            
+            //List of commands to Haas mill/lathe
+            List<string> Commands = new List<string> {"Q100", "Q102"};
+
+            HaasConnection hc = new HaasConnection(IPAddr, Port);
+
+            for (int i = 0; i < Commands.Count; i++)
+            {
+                if (hc.IsConnected)
+                {
+                    // send command to instrument
+                    hc.TermWrite(Commands[i] + "\r\n");
+
+                    // display instrument reply
+                    string srvmsg = hc.TermRead();
+                    //Remove the interactive prompt
+                    if (srvmsg.EndsWith(">")) srvmsg = srvmsg.TrimEnd('>'); 
+                    Console.Write(srvmsg);
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press a key to exit.");
+
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
